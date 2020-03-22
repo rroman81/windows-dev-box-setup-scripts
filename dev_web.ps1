47ef45eebbd64b02c699ddfc80400a3108004f07
Roman Rozinov <rroman81@gmail.com>
2020-03-21 20:51:02 -0700

    added vs2019ent to the dev web workstation

--- dev_web.ps1
+++ dev_web.ps1
@@ -35,6 +35,24 @@ executeScript "Browsers.ps1";
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension msjsdiag.debugger-for-edge
 
+#--- Tools ---
+#--- Installing VS and VS Code with Git
+# See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
+# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
+# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
+# visualstudio2017community
+# visualstudio2017professional
+# visualstudio2017enterprise
+
+choco install -y visualstudio2019enterprise --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+Update-SessionEnvironment #refreshing env due to Git install
+
+#--- Installing Windows Template Studio ---
+choco install -y visualstudio2019-workload-azure
+choco install -y visualstudio2019-workload-manageddesktop
+choco install -y visualstudio2019-workload-office
+choco install -y visualstudio2019-workload-data
+
 #--- Microsoft WebDriver ---
 choco install -y microsoftwebdriver
 
