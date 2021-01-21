57376418e90db523580ec838ac3a10135b78e1d4
Roman Rozinov <rroman81@gmail.com>
2021-01-21 00:35:03 -0700

    added common dev tools

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,8 +1,19 @@
 
 # tools we expect devs across many scenarios will want
+choco install -y pwsh
+choco install -y nvm
 choco install -y vscode
+choco install -y microsoft-windows-terminal
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y poshgit
+choco install -y gh
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
 choco install -y ditto
+choco install -y textpad
+choco install -y slack
+choco install -y teamviewer
+choco install -y keepass
+
+
